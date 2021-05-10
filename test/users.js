e75f663b8e226cc3705ccaca2b6aa79c70e65cdb
Andrew Bustos <stevandrew@Andrews-MacBook-Pro.local>
2021-05-09 23:27:45 -0600

    added create user and update user test

--- test/users.js
+++ test/users.js
@@ -4,10 +4,37 @@ const chai = require('chai');
 const expect = chai.expect;
 
 describe('Users', () => {
-    it('Get /api/users', (done) => {
-        request.get('/api/users/').end((err, res) => {
+    let userId;
+    it('GET /api/users', () => {
+        return request.get('/api/users/').then((res) => {
             expect(res.body).to.not.be.empty;
-            done();
+        })
+    });
+
+    it('POST /api/users', () => {
+
+        const data = {
+            name: "Test Dummy",
+            username: "test.dummy",
+            password: "pass"
+        };
+
+        return request.post('/api/users')
+        .send(data)
+        .then((res) => {
+            expect(res.body).to.deep.include(data);
+            userId = res.body.id;
+        });
+    });
+
+    it('PUT /api/users/:Id', () => {
+        const data = {
+            name: "Mr. Dummy"
+        }
+        return request.put(`/api/users/${userId}`)
+        .send(data)
+        .then((res) => {
+            expect(res.body.message).to.equal("User was updated successfully.");
         });
     });
 });
