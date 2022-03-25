06c1e6b4386a95361c8e92b9a5866500e87658d8
Andrew Bustos <andybustos021@gmail.com>
2022-03-24 20:39:14 -0600

    Updated server.py to include sms and serial listening fuctionality; also added example.env config file

--- RPI/server.py
+++ RPI/server.py
@@ -6,31 +6,60 @@
 ######################################
 
 import serial
-import time
+import requests
 import os
+import sys
+import distutils.util
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
-serialPort = os.environ.get('SERIAL_PORT')
-baudrate = int(os.environ.get('BAUDRATE'))
+def alert():
+    TEXTBELT_API_KEY = ""
+    isTest = True
+    msg = "Test message"
+    number = os.environ.get("PHONE_NUMBER")
 
-print(serialPort, baudrate)
-
-def main():
-    # Serial( PORT, Baudrate)
-    ser=serial.Serial(serialPort, baudrate)
-
-    signal = ser.readline()
-
-    if signal == "water-shutoff":
-        print('sent sms!')
+    if isTest:
+        TEXTBELT_API_KEY = os.environ.get("TB_API_KEY") + "_test"
     else:
-        print('invalid serial signal recieved: %s' % signal)
+        TEXTBELT_API_KEY = os.environ.get("TB_API_KEY")
 
-    print(signal)
-    ser.close()
+    print(TEXTBELT_API_KEY)
+
+    res = requests.post("https://textbelt.com/text", {
+        'phone': number,
+        'message': msg,
+        'key': TEXTBELT_API_KEY,
+    })
+
+    print(res.json())
+
+    sys.exit()
+
+def loop():
+    BAUDRATE = os.environ.get("BAUDRATE")
+    ser = serial.Serial('/dev/cu.usbserial-1420',
+    baudrate = BAUDRATE,
+    parity = serial.PARITY_NONE,
+    stopbits = serial.STOPBITS_ONE,
+    bytesize = serial.EIGHTBITS,
+    )
+
+    isTriggered = False
+
+    while True:
+        data = ser.read_until(b'\r').decode('utf-8', 'ignore').strip('\r').replace(" ", "")
+        arr = data.split(',')
+
+        strBool = distutils.util.strtobool(arr[3])
+        print(arr)
+        isTriggered = bool(strBool)
+
+        if isTriggered:
+            alert()
+
+        print(data)
 
 if __name__ == "__main__":
     while True:
-        main()
-        time.sleep(5)
+        loop()
